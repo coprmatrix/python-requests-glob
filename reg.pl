f729fc1cd541fe71ed36fe64de9620f553672f15
u <u>
2025-03-29 21:46:55 +0300

    update

--- reg.pl
+++ reg.pl
@@ -2,6 +2,7 @@ my $name = <<'EOF';
 %files -n %{python_name}
 %{python3_sitelib}/requests_glob-%{version}.dist-info
 %{python3_sitelib}/requests_glob.py
+%{python3_sitelib}/__pycache__/*
 EOF
 
 s/%files -n %{python_name}.*/${name}/g;
